ff4d3c8a078a29312c009156f552dec6ac521770
Zach Wilcher <mail@zachwilcher.com>
2022-07-08 10:38:31 -0400

    Added some seeded messages.

--- packages/emberblog-api/app/configs/mongodb.js
+++ packages/emberblog-api/app/configs/mongodb.js
@@ -1,7 +1,10 @@
+const { env } = require('@onehilltech/blueprint');
+
 module.exports = {
     connections: {
         $default: {
-            uri: `mongodb://localhost:27017/emberblog`, // process.env.mongodb_uri,
+            uri: `mongodb://localhost:27017/emberblog_${env}`, // process.env.mongodb_uri,
+            seed: false,
             options : { // mongoose connection options
                 useUnifiedTopology: true, // removes warning about Current Server Discovery and Monitoring engine being deprecated
                 family: 4 // so localhost works instead of 127.0.0.1
